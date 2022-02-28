675da570acf4456649ecc1a2f8f368ed11cbc993
abulbashar98 <abulbashar98116@gmail.com>
2022-02-28 23:55:37 +0600

    Integrate chrome devtool console with VS Code

--- fetch.js
+++ fetch.js
@@ -10,3 +10,10 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(json => console.log(json));
 console.log(3333333);
 console.log(444444);
+
+// Note: To integrate Chrome dev Tool with JS...
+
+// Step 1: Open live server from HTML file.
+// step 2: Open Run and debug in HTML.
+// step 3: Open Debug Console.
+// step 4: Now Chrome Dev tool and Debug Console is Connected.
